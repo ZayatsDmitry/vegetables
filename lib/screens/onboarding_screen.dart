d9acba889a1bef4f9a8a8820d0e44933e963b5a4
ZayatsDmitry <108630172+ZayatsDmitry@users.noreply.github.com>
2023-12-24 02:53:48 +0300

    add new branch onBoarding (#2)
    * add new branch onBoarding
    
    * add onboarding screen (don't finish)

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:vegatables/data/data.dart';
+import 'package:vegatables/data/start_button_widget.dart';
 
 class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
@@ -8,16 +10,97 @@ class OnBoardingScreen extends StatefulWidget {
 }
 
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
+
+  int currentPage = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.95,
-            height: MediaQuery.of(context).size.height * 0.85,
-          ),
+        child: PageView.builder(
+          itemCount: vegetables.length,
+          itemBuilder: (context, index) {
+            return Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.95,
+                height: MediaQuery.of(context).size.height * 0.85,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  // color: Colors.black,
+                  gradient: LinearGradient(
+                      colors: vegetables[index].gradientColors,
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset("assets/${vegetables[index].image}.png"),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      vegetables[index].title,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: Text(
+                        vegetables[index].headline,
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 80,
+                    ),
+                    currentPage == 3 ? const StartButton() : Container(),
+
+                    currentPage == 3 ? const SizedBox(height: 20,) : Container(),
+
+                    buildPageIndicator()
+                  ],
+                ),
+              ),
+            );
+          },
+          onPageChanged: (int page) {
+            setState(() {
+              currentPage = page;
+            });
+          },
         ),
+      ),
+    );
+  }
+
+  Widget buildPageIndicator() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List<Widget>.generate(
+        vegetables.length,
+        (index) {
+          return Container(
+            width: 10,
+            height: 10,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 5,
+            ),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: currentPage == index ? Colors.white : Colors.grey.shade600,
+            ),
+          );
+        },
       ),
     );
   }
